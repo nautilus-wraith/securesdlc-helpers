da8760f6dcf11e01992bd7681d9836f43830b7db
jkosik-832gh8qwebc8 <132492430+jkosik-832gh8qwebc8@users.noreply.github.com>
2025-07-20 17:13:12 +0200

    slack notif

--- .github/actions/healthcheck/notify.sh
+++ .github/actions/healthcheck/notify.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-WORKFLOW_NAME="$1"
-STEP_CONCLUSION="$2"
-REPOSITORY="$3"
-RUN_ID="$4"
+WORKFLOW="$1"
+REPOSITORY="$2"
+RUN_ID="$3"
+STEP_CONCLUSION="$4"
 SLACK_WEBHOOK="$5"
 
 echo "Sending Slack notification for failed steps."
@@ -14,7 +14,7 @@ ATTACHMENT=$(cat <<EOF
   "attachments": [
     {
       "color": "${COLOR}",
-      "title": "SecureSDLC GitHub Workflow ${WORKFLOW_NAME} state: ${STEP_CONCLUSION}",
+      "title": "SecureSDLC GitHub Workflow ${WORKFLOW} state: ${STEP_CONCLUSION}",
       "title_link": "https://github.com/${REPOSITORY}",
       "fields": [
         {
